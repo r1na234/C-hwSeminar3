c095c1ea95c80d830b658e9e0bf87cb78edd8022
Katerine <frna338@gmail.com>
2022-09-30 19:09:27 +0300

    Доп задача 2 сем

---
+++ доп задача 2 семинар/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+
+System.Console.WriteLine("Enter the number");
+int number = int.Parse(Console.ReadLine());
+
+if(((number % 7) == 0) && ((number % 23) == 0)){
+        System.Console.WriteLine("кратно 7 и 23");
+}
+else{
+    System.Console.WriteLine("не кратно");
+}
